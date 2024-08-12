4fe5a60d3ee6d3d76da7b1ef291e04f1be348ad9
khushiR8 <beedassykhushi@gmail.com>
2024-08-12 21:11:43 +0400

    ex8 added

--- Computer Programming/labsheet4/ex7.py
+++ Computer Programming/labsheet4/ex7.py
@@ -6,5 +6,5 @@ if human_years <= 2:
     dog_years = human_years*10.5
     print("Dog years :",dog_years)
 else:
-    dog_years =  (2*10.5) + ((human_years -2)*4)
+    dog_years =  (2*10.5) + ((human_years-2)*4)
     print("Dog years :",dog_years)
