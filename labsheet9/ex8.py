c6680ac6e9cc9fd6c254e29768903de7abc4f3b1
khushiR8 <beedassykhushi@gmail.com>
2024-10-11 21:00:25 +0400

    modifies

--- labsheet9/ex8.py
+++ labsheet9/ex8.py
@@ -4,6 +4,10 @@ def area_Circle(radius):
     area=math.pi*(radius**2)
     return area
 
-radius=float(input('enter radius:'))
-area=area_Circle(radius)
-print(area)
+def main():
+    radius=float(input('enter radius:'))
+    area=area_Circle(radius)
+    print(area)
+    
+main()    
+    
